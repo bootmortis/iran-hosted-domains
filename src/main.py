53a340033d99d7266a78da734075a198cd497123
Tirbod <65743522+Tirbod@users.noreply.github.com>
2023-05-19 23:05:07 +0330

    Refine Surge Rules
    Changing Surge Rules to be same as Clash.

--- src/main.py
+++ src/main.py
@@ -61,5 +61,5 @@ if __name__ == "__main__":
     create_config.qv2ray(other_domains, proxy_domains, ad_domains)
     create_config.shadowrocket(all_domains)
     create_config.clash(other_domains, ad_domains)
-    create_config.surge(all_domains)    
+    create_config.surge(other_domains, ad_domains)   
     create_config.switchy_omega(other_domains)
