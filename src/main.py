9be5ed1210793d23688b90d284590de30761fe9f
chocolate4u <133856779+chocolate4u@users.noreply.github.com>
2023-09-22 12:17:39 +0330

    update_patch1
    reverted changes to 'ir' category
    added 'tld_ir' and 'all' categories

--- src/main.py
+++ src/main.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
 
     # Generate output files
     utils.save_to_file(consts.ad_domains_path, "\n".join(ad_domains))
+    utils.save_to_file(consts.ir_domains_path, "\n".join(ir_domains))
     utils.save_to_file(consts.other_domains_path, "\n".join(other_domains))
     utils.save_to_file(consts.all_domains_path, "\n".join(all_domains))
 
